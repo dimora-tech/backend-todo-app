831aeec7450e258a2f03ccb4bd74a4c9e587220f
dimora-tech <111430150+dimora-tech@users.noreply.github.com>
2026-04-06 02:42:43 +0100

    Update admin.py

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -3,3 +3,10 @@ from .models import TodoItem
 
 # Register your models here.
 admin.site.register(TodoItem)
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'task', 'completed')
+    search_fields = ('task',)
+    list_filter = ('completed',)
+    ordering = ('-id',)
+    list_editable = ('completed',)
