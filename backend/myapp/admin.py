8efa279c5482561a7801e4d6b71065d6004b767c
dimora-tech <111430150+dimora-tech@users.noreply.github.com>
2026-04-06 02:46:38 +0100

    Update admin.py

--- backend/myapp/admin.py
+++ backend/myapp/admin.py
@@ -2,8 +2,7 @@ from django.contrib import admin
 from .models import TodoItem
 
 # Register your models here.
-admin.site.register(TodoItem)
-
+@admin.register(TodoItem)
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('id', 'task', 'completed')
     search_fields = ('task',)
