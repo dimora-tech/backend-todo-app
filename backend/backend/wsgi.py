5728f954ca64775d488f78fe1e328c49adff907d
dimora-tech <111430150+dimora-tech@users.noreply.github.com>
2026-04-06 01:46:52 +0100

    Update wsgi.py

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 try:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
-except ModuleNotFoundError:
+except:
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend.settings')
 application = get_wsgi_application()
